3c160e80330c602a47a0beae41399ff5da999932
Zoe Henry <zoe.henry@yale.edu>
2017-07-15 16:18:24 -0700

    added expense log page and record new expense functionality

--- app/src/main/java/com/streetsell/app/streetsell/RecordExpenseActivity.java
+++ app/src/main/java/com/streetsell/app/streetsell/RecordExpenseActivity.java
@@ -1,13 +1,10 @@
 package com.streetsell.app.streetsell;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -15,32 +12,24 @@ import android.widget.Toast;
  * Created by zoehenry on 7/15/17.
  */
 
-public class ExpenseFragment extends Fragment {
+public class RecordExpenseActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+        setContentView(R.layout.activity_record_expense);
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        final View expenseView = inflater.inflate(R.layout.fragment_expense, container, false);
-        Button save = (Button)expenseView.findViewById(R.id.save);
+        Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(new View.OnClickListener() {
-        Context context = getActivity();
 
             @Override
             public void onClick(View view) {
                 CharSequence text = "Your expenses have been saved!";
                 int duration = Toast.LENGTH_SHORT;
 
-                Toast toast = Toast.makeText(context, text, duration);
+                Toast toast = Toast.makeText(getBaseContext(), text, duration);
                 toast.setGravity(Gravity.TOP, 0, 450);
                 toast.show();
             }
         });
-        return expenseView;
     }
-
 }
